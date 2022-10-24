76b28d8f7666e7431e5dcf4db03bf27b62bf396f
thekoksaloktar <116364185+thekoksaloktar@users.noreply.github.com>
2022-10-24 11:45:25 +0200

    Add files via upload

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Enter1_number, Enter2_number ;
+
+            double result;
+
+            string process;
+
+            Console.WriteLine("Enter a number:");
+            Enter1_number = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter second number:");
+            Enter2_number = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("choose process(*,/,+,-)");
+            process = Console.ReadLine();
+
+            if (process == "*")
+            {
+                result = Enter1_number * Enter2_number;
+                Console.WriteLine("Result: " + result);
+            }
+            if (process == "/")
+            {
+                result = Enter1_number / Enter2_number;
+                Console.WriteLine("Result: " + result);
+            }
+            if (process == "+")
+            {
+                result = Enter1_number + Enter2_number;
+                Console.WriteLine("Result: " + result);
+            }
+            if (process == "-")
+            {
+                result = Enter1_number - Enter2_number;
+                Console.WriteLine("Result: " + result);
+            }
+        }
+    }
+}
